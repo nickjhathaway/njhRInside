d12994693a25a5a234040dac2d49b60aae71ef6c
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-05-20 19:54:04 -0400

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char* argv[]){
     std::cout << "Hello njhRInside!" << std::endl;
     njhRInside::OwnRInside rSes;
     auto & R = rSes.get();
-    R.parseEvalQ("print (1:10)");
+    R.parseEvalQ("print (\"Hello from inside of R\")");
     return 0;
 }
