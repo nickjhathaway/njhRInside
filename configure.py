d5c1fd762cd40e80109363a4ccf46c7ccf6eb82b
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-05-23 16:53:52 -0400

    major update to how the setup scripts are organized;

--- configure.py
+++ configure.py
@@ -4,31 +4,12 @@ import shutil, os, argparse, sys, stat
 from setUpScripts.utils import Utils
 from setUpScripts.genFuncs import genHelper
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-prefix', type=str, nargs=1)
-    parser.add_argument('-externalLibDir', type=str, nargs=1)
-    parser.add_argument('-CC', type=str, nargs=1)
-    parser.add_argument('-CXX', type=str, nargs=1)
-    return parser.parse_args()
-
 def main():
-    args = parse_args()
-    prefix = "";
-    external = "external";
-    CC = genHelper.determineCC(args)
-    CXX = genHelper.determineCXX(args)
-    if(args.externalLibDir):
-        external = args.externalLibDir[0];
-    
-    if(args.prefix):
-        prefix = args.prefix[0];
-        cmd = "setUpScripts/generateCompFile.py -prefix "  + prefix + " -installName njhRInside -outFilename compfile.mk -externalLoc " + external + " -CC " + CC + "  -CXX " + CXX + " -neededLibs cppitertools,r -outname njhRInside "
-
-    else:
-        cmd = "setUpScripts/generateCompFile.py "  + " -installName njhRInside -outFilename compfile.mk -externalLoc " + external + " -CC " + CC + "  -CXX " + CXX  + "  -neededLibs cppitertools,r -outname njhRInside "
-
-    
+    name = "njhRInside"
+    libs = "cppitertools,r"
+    args = genHelper.parseNjhConfigureArgs()
+    cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
     
 main()
+
