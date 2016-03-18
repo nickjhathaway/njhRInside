8ee2aa3febb3e20e52ca4279ebb46d086a1b8e4c
Nicholas Hathaway <nickjhathaway@gmail.com>
2016-03-18 16:39:45 -0400

    new setup;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "njhRInside"
-    libs = "cppitertools,r"
+    libs = "cppitertools:v0.1,r:3.2.4"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
